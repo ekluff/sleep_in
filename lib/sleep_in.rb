2c6ddaa0b2582385b05f8d1f11c322f86623be50
Evan Clough & Leah Klamkin <student@epicodus.com>
2015-08-04 11:06:14 -0700

    Add functionality to return messages for weekday dates.

--- lib/sleep_in.rb
+++ lib/sleep_in.rb
@@ -4,8 +4,20 @@ class Time
 
     if day_number == 0 || day_number == 6
       "You can sleep in!"
+    elsif day_number == 1
+      "Monday"
+    elsif day_number == 2
+      "Tuesday"
+    elsif day_number == 3
+      "Wednesday"
+    elsif day_number == 4
+      "Thursday"
+    elsif day_number == 5
+      "Friday"
+    else
+      "What alien world is this that has no dates?" #My first Easter egg :^)
     end
-    
+
   end
 
 end
--- spec/sleep_in_spec.rb
+++ spec/sleep_in_spec.rb
@@ -3,16 +3,21 @@ require('sleep_in')
 
 describe('Time#sleep_in') do
 
-  it('returns the number of the day of the week for the given date') do
-    expect(Time.new(2015,8,4).sleep_in).to(eq(2))
-  end
-
-  it('returns true for weekend days and false for weekday days') do
-    expect(Time.new(2015,8,1).sleep_in).to(eq(true))
-  end
+  # it('returns the number of the day of the week for the given date') do
+  #  expect(Time.new(2015,8,4).sleep_in).to(eq(2))
+  # end
+  #
+  # it('returns true for weekend days and false for weekday days') do
+  #  expect(Time.new(2015,8,1).sleep_in).to(eq(true))
+  # end
+  # Commented above because they are interim tests that now fail by design.
 
   it('returns "You can sleep in!" for weekend days') do
     expect(Time.new(2015,8,1).sleep_in).to(eq("You can sleep in!"))
+  end
+
+  it('returns the name of the day of the week for weekday days') do
+    expect(Time.new(2015,8,4).sleep_in).to(eq("Tuesday"))
   end
 
 end
