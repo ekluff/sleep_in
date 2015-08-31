7deffbf0df6dee57fc35945cb0595c08d7ec260a
Evan Clough <clough.evan@gmail.com>
2015-08-31 11:19:54 -0700

    Delete specs for early development steps, fail by design in full buildout.

--- spec/sleep_in_spec.rb
+++ spec/sleep_in_spec.rb
@@ -3,15 +3,6 @@ require('sleep_in')
 
 describe('Time#sleep_in') do
 
-  # it('returns the number of the day of the week for the given date') do
-  #  expect(Time.new(2015,8,4).sleep_in).to(eq(2))
-  # end
-  #
-  # it('returns true for weekend days and false for weekday days') do
-  #  expect(Time.new(2015,8,1).sleep_in).to(eq(true))
-  # end
-  # Commented above because they are interim tests that now fail by design.
-
   it('returns "You can sleep in!" for weekend days') do
     expect(Time.new(2015,8,1).sleep_in).to(eq("You can sleep in!"))
   end
